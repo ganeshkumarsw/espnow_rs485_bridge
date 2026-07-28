638a0f643d38fcfaeb17849787b5a1dda62d7df2
ganeshkumarsw <ganeshkumarsw@gmail.com>
2026-07-28 15:43:06 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/pm/pm.h>
+#include <zephyr/pm/device.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    while (1)
+    {
+       	printf("World! %s\n", CONFIG_BOARD_TARGET);
+        k_msleep(100);
+    }
+    
+}
